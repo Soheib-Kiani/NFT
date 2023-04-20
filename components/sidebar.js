52c4e32ab73427905e5bc5a9e108cecdcfb265f6
soheib kiani <mrsoheibkiani@gmail.com>
2023-04-20 16:56:55 +0430

    Finish Sidebar for lg

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,13 +1,13 @@
 /* eslint-disable @next/next/no-img-element */
 const sidebar = () => {
   return (
-    <div className="h-screen bg-black w-52 font-jost">
+    <div className="min-h-screen bg-black text-sm w-52 font-jost">
       {/* Sidebar Icon */}
-      <div className="grid place-items-center pt-5">
+      <div className="grid place-items-center pt-3">
         <img src="/side-icon/Group 2.png" alt="img" />
       </div>
-      {/* Market plavce Buttons */}
-      <p className="text-white font-extralight mt-7 uppercase">Marketplace</p>
+      {/* Marketplace Buttons */}
+      <p className="text-white pl-1 font-extralight mt-5 uppercase">marketplace</p>
       <div className="grid place-items-center gap-y-1">
         {/* #1 */}
         <div className="flex w-full mt-3 items-start px-5 gap-x-3 text-center">
@@ -25,6 +25,62 @@ const sidebar = () => {
           <p className="text-white">Favourite</p>
         </div>
       </div>
+      {/* ____________ */}
+      {/* Create Buttons */}
+      <p className="text-white pl-1 font-extralight mt-5 uppercase">create</p>
+      <div className="grid place-items-center gap-y-1">
+        {/* #1 */}
+        <div className="flex w-full mt-3 items-start px-5 gap-x-3 text-center">
+          <img src="/side-icon/Plus.png" alt="img" />
+          <p className="text-white">Upload New</p>
+        </div>
+        {/* #2 */}
+        <div className="flex w-full mt-3 items-start px-5 gap-x-3 text-center">
+          <img src="/side-icon/Download.png" alt="img" />
+          <p className="text-white">Import NFT</p>
+        </div>
+        {/* #3 */}
+        <div className="flex w-full mt-3 items-start px-5 gap-x-3 text-center">
+          <img src="/side-icon/Paper Negative.png" alt="img" />
+          <p className="text-white">Create Empty Kit</p>
+        </div>
+      </div>
+      {/* ____________ */}
+      {/* Playlist Buttons */}
+      <p className="text-white pl-1 font-extralight mt-5 uppercase">create</p>
+      <div className="grid place-items-center gap-y-1">
+        {/* #1 */}
+        <div className="flex w-full mt-3 items-start px-5 gap-x-3 text-center">
+          <img src="/side-icon/Play.png" alt="img" />
+          <p className="text-white">Sample Music</p>
+        </div>
+        {/* #2 */}
+        <div className="flex w-full mt-3 items-start px-5 gap-x-3 text-center">
+          <img src="/side-icon/Voice.png" alt="img" />
+          <p className="text-white">Your Top Songs</p>
+        </div>
+        {/* #3 */}
+        <div className="flex w-full mt-3 items-start px-5 gap-x-3 text-center">
+          <img src="/side-icon/Discovery.png" alt="img" />
+          <p className="text-white">Discover Weekly</p>
+        </div>
+      </div>
+      {/* ____________ */}
+      {/* General Buttons */}
+      <p className="text-white pl-1 font-extralight mt-11 uppercase">general</p>
+      <div className="grid place-items-center gap-y-1">
+        {/* #1 */}
+        <div className="flex w-full mt-3 items-start px-5 gap-x-3 text-center">
+          <img src="/side-icon/Setting.png" alt="img" />
+          <p className="text-white">Setting</p>
+        </div>
+        {/* #2 */}
+        <div className="flex w-full mt-3 items-start px-5 gap-x-3 text-center">
+          <img src="/side-icon/Logout.png" alt="img" />
+          <p className="text-white">Sign out</p>
+        </div>
+      </div>
+      {/* ____________ */}
     </div>
   );
 };
